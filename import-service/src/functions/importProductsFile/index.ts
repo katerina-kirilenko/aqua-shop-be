8a153480654886dfe276fa3a63ecdc06e21dc23a
Katsiaryna_Kirylenka <Katsiaryna_Kirylenka@epam.com>
2022-11-14 14:38:08 +0100

    task7: add Authorization

--- import-service/src/functions/importProductsFile/index.ts
+++ import-service/src/functions/importProductsFile/index.ts
@@ -8,6 +8,13 @@ export const importProductsFile = {
         method: 'get',
         path: 'import',
         cors: true,
+        authorizer: {
+          name: 'basicAuthorizer',
+          arn: 'arn:aws:lambda:eu-central-1:083525352146:function:authorization-service-dev-basicAuthorizer',
+          resultTtlInSeconds: 0,
+          identitySource: 'method.request.header.Authorization',
+          type: 'token',
+        }
       },
     },
   ],
